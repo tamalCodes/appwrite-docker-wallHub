fe300b2a970ef11ff9ef1aa12c567f0f76ee6896
Tamal Das <gyansujan69@gmail.com>
2022-04-18 11:04:57 +0530

    fix : login, signup linked with appwrite

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Loginpic from "../assets/Login.svg"
 import "../styles/Login.css"
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { account } from "../services/Appwriteconfig";
 
 const Login = () => {
 
   const [creds, setcreds] = useState({ email: "", password: "" });
+  const [uderdetails, setuderdetails] = useState();
+
+  const history = useHistory()
+
+
+
+  const handleChange = (e) => {
+    setcreds({
+      ...creds,
+      [e.target.name]: e.target.value,
+    });
+
+
+  };
+
+  //onclick event for signup
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await account.createSession(creds.email, creds.password);
+    const user = await account.get();
+    console.log(user);
+
+  };
+
+
+  const handlelogout = async (e) => {
+    e.preventDefault();
+    await account.deleteSessions();
+
+  };
+
+
   return (
     <>
 
@@ -18,10 +52,35 @@ const Login = () => {
         <div className="bodytext_div">
           <h1 className='login_header'>Login to WallHub</h1>
           <form className='login_form'>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Email adress' />
-            <input type="password" className="form-control" id="exampleInputPassword1" placeholder='Password' />
 
-            <button type="button" class="btn btn-warning">Submit</button>
+            <input
+              onChange={(e) => {
+                handleChange(e)
+              }}
+              type="email"
+              className="form-control"
+              id="email"
+              required
+              aria-describedby="email"
+              name="email"
+              value={creds.email}
+            />
+
+            <input
+              onChange={(e) => {
+                handleChange(e)
+              }}
+              required
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={creds.password}
+            />
+
+            <button type="button" class="btn btn-warning" onClick={(e) => { handleSubmit(e) }}>Submit</button>
+
+            <button type="button" class="btn btn-danger" onClick={(e) => { handlelogout(e) }}>Submit</button>
             <p>Don't have an account ? <Link to={"/signup"}>Signup</Link> here</p>
           </form>
 
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,114 +1,3 @@
-// import React, { useState } from "react";
-// import SocialSignin from "./SocialSignin";
-// import { Link } from "react-router-dom";
-// import { account } from "../services/Appwriteconfig";
-
-// const Signup = () => {
-
-//   const [userDetails, setUserDetails] = useState({
-//     name: "",
-//     email: "",
-//     password: "",
-//   });
-
-//   const signupUser = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const newuser = await account.create('unique()', userDetails.email, userDetails.password, userDetails.name);
-//       console.log(newuser);
-//     } catch (error) {
-//       console.log(error);
-//     }
-
-//   };
-
-//   return (
-//     <div>
-//       <h2 className="mt-5 text-center">Super Auth</h2>
-//       <h3 className=" text-center">Signup</h3>
-
-//       <form className="container">
-//         <div className="mb-3">
-//           <label for="name" className="form-label">
-//             Name
-//           </label>
-//           <input
-//             onChange={(e) => {
-//               setUserDetails({
-//                 ...userDetails,
-//                 name: e.target.value,
-//               });
-//             }}
-//             type="text"
-//             className="form-control"
-//             id="name"
-//             required
-//             aria-describedby="name"
-//             name="name"
-//           />
-//         </div>
-//         <div className="mb-3">
-//           <label for="email" className="form-label">
-//             Email address
-//           </label>
-//           <input
-//             onChange={(e) => {
-//               setUserDetails({
-//                 ...userDetails,
-//                 email: e.target.value,
-//               });
-//             }}
-//             type="email"
-//             className="form-control"
-//             id="email"
-//             required
-//             aria-describedby="email"
-//             name="email"
-//           />
-//         </div>
-//         <div className="mb-3">
-//           <label for="password" className="form-label">
-//             Password
-//           </label>
-//           <input
-//             onChange={(e) => {
-//               setUserDetails({
-//                 ...userDetails,
-//                 password: e.target.value,
-//               });
-//             }}
-//             required
-//             type="password"
-//             className="form-control"
-//             id="password"
-//             name="password"
-//           />
-//         </div>
-//         <div className="mb-3">
-//           <span>Already have an account ? </span>{" "}
-//           <Link to="/login">
-//             <button className="btn btn-primary">Login</button>
-//           </Link>
-//         </div>
-
-//         <button
-//           onClick={(e) => signupUser(e)}
-//           type="submit"
-//           className="btn btn-success"
-//         >
-//           Signup
-//         </button>
-//       </form>
-
-//       <SocialSignin />
-
-//     </div>
-//   );
-// };
-
-// export default Signup;
-
-
 import React, { useState } from 'react'
 import Registerpic from "../assets/Register.svg"
 import "../styles/Login.css"
@@ -116,9 +5,11 @@ import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import { account } from "../services/Appwriteconfig";
+import { useHistory } from 'react-router-dom';
 
 const Signup = () => {
   const [creds, setcreds] = useState({ name: "", email: "", password: "" });
+  const history = useHistory()
 
   //onchange event for email and password
   const handleChange = (e) => {
@@ -136,6 +27,8 @@ const Signup = () => {
     try {
       const newuser = await account.create('unique()', creds.email, creds.password, creds.name);
       console.log(newuser);
+
+      history.push('/login')
     } catch (error) {
       console.log(error);
     }
